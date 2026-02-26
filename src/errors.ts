9c763c610c356a0d8467360ddb000ca62cf803ba
berkayyildirim <hbenyork@gmail.com>
2026-02-26 17:12:50 +0000

    fix: internalize JWK conversion and add missing error classes

--- src/errors.ts
+++ src/errors.ts
@@ -4,3 +4,17 @@ export class AgentAuthError extends Error {
     this.name = 'AgentAuthError'
   }
 }
+
+export class ConfigurationError extends AgentAuthError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'ConfigurationError'
+  }
+}
+
+export class TokenValidationError extends AgentAuthError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'TokenValidationError'
+  }
+}
